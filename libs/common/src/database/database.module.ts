a31e121307747e71e846e7ef8e2b33e13c8cdc71
myukang <myunghwan0421@gmail.com>
2023-08-20 03:41:56 +0900

    :sparkles: feat: add mongoose module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import {MongooseModule} from "@nestjs/mongoose";
 
-@Module({})
+@Module({
+    imports: [MongooseModule.forRoot('mongodb://mongo:27017/ms-nest')],
+})
 export class DatabaseModule {}
